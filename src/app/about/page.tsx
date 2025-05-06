b1852df06493fa0d74e97a573cf5361d41d2327f
Site Wang <sitedashwang@gmail.com>
2025-05-05 23:21:44 -0700

    initial change

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <section style={{ padding: 20 }}>
+        <h2>About Me</h2>
+        <p>I’m a Python-loving ML engineer who’s learning the modern web stack.</p>
+      </section>
+    )
+  }
